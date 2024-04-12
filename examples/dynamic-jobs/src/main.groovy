16118ed957bd1752c3aabf19996ce3993ae5cb3a
rohit200207 <rohit.d@softwaremathematics.com>
2024-04-13 00:37:47 +0530

    dynamic jobs

--- examples/dynamic-jobs/src/main.groovy
+++ examples/dynamic-jobs/src/main.groovy
@@ -1,1 +1,29 @@
-println("Namaste!")
+@Grab('org.yaml:snakeyaml:1.17')
+import org.yaml.snakeyaml.Yaml
+import hudson.model.*
+
+// function to search for all the files ending with .yaml or .yml
+ArrayList searchYamlFiles(String dirPath) {
+    ArrayList file_list = []
+    File dir = new File(dirPath)
+    if (dir.isDirectory()) {
+        dir.eachFileRecurse { file ->
+            if (file.isFile() && (file.name.endsWith(".yaml") || file.name.endsWith(".yml"))) {
+                file_list.add(file.absolutePath)
+            }
+        }
+    } else {
+        println("Error: $dirPath is not a valid directory.")
+    }
+    return file_list
+}
+
+// get the current working directory
+def cwd = hudson.model.Executor.currentExecutor().getCurrentWorkspace().absolutize()
+
+pipeline_file_list  = searchYamlFiles(cwd.toString())
+
+for (current_pipeline in pipeline_file_list) {
+
+    println("Working on :" + current_pipeline)
+}
