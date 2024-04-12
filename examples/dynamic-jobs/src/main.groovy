6438c1ee7f16beec396e15ed1f18032da7a99096
rohit200207 <rohit.d@softwaremathematics.com>
2024-04-13 00:45:24 +0530

    dynamic jobs

--- examples/dynamic-jobs/src/main.groovy
+++ examples/dynamic-jobs/src/main.groovy
@@ -26,4 +26,10 @@ pipeline_file_list  = searchYamlFiles(cwd.toString())
 for (current_pipeline in pipeline_file_list) {
 
     println("Working on :" + current_pipeline)
+
+    //parsing the yaml content
+    parsed_job_config = new Yaml().load((current_pipeline as File).text)
+
+    print("jobname is: "+parsed_job_config.job_name)
+
 }
