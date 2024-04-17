c2fdd7251180a67f6bce7f2c8417288e23c76726
rohit200207 <rohit.d@softwaremathematics.com>
2024-04-18 00:01:27 +0530

    dynamic jobs

--- examples/dynamic-jobs/src/utils/JobUtils.groovy
+++ examples/dynamic-jobs/src/utils/JobUtils.groovy
@@ -6,15 +6,19 @@ import org.yaml.snakeyaml.Yaml
 class JobUtils{
 
     def job_config
+    def file_path
 
     JobUtils(current_pipeline){
         //parsing the yaml content
         def parsed_job_config = new Yaml().load((current_pipeline as File).text)
         this.job_config=parsed_job_config
+        this.file_path=current_pipeline
     }
 
     String get_job_name(){
-        return this.job_config.job_name
+        String slash_split= this.file_path.split("/")[-1]
+        String job_name= slash_split.split {".yaml"}[0].split{".yml"}[0]
+        return job_name
     }
 
     String get_build_command(){
